8888203856127a329d3f7e985bfd53e481125ec1
Almas-dev-ops <almas.nazarbek01@gmail.com>
2025-05-20 22:02:40 +0500

    easy w5 готов. medium w5 готов. hard w5 готов

---
+++ Module_5/Homework/hard/src/MainW5Hard.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+public class MainW5Hard
+{
+    public static void main(String[] args)
+    {
+       ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("Введите цифры: ");
+        Scanner input = new Scanner(System.in);
+        int num;
+        while ((num = input.nextInt()) > 0){
+
+            list.add(num);
+            System.out.println("Изначальный список: "+ list);
+
+            LinkedHashSet<Integer> hashSet = new LinkedHashSet<>(list);
+            ArrayList<Integer> listRemoveDublicates;
+            listRemoveDublicates= new ArrayList<>(hashSet);
+
+            System.out.println("Список с удаленными дубликатами,\nи с сохраненным порядком элементов: "+listRemoveDublicates);
+        }
+        input.close();
+
+    }
+}
+
+
